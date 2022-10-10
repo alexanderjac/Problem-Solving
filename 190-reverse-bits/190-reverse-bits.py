cb7d72d5a977d825f98fc8710188c1a3181c9d02
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-10-09 20:05:57 -0400

    Time: 59 ms (47.00%), Space: 13.9 MB (50.14%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ret , power = 0,31
+        while n :
+            ret += (n&1)<<power
+            n = n>>1
+            power -=1
+        return ret
+        

c5c5cbcbd378b2196a1cc752f1bc481be21a8d51
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-10-10 14:49:50 -0400

    Time: 42 ms (90.06%), Space: 14.3 MB (45.67%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        result =[]
+        parent_hash = {}
+        queue = [root]
+        while queue:
+            curr_node = queue.pop(0)
+            if curr_node.left:
+                queue.append(curr_node.left)
+                parent_hash[curr_node.left] = curr_node
+            if curr_node.right:
+                queue.append(curr_node.right)
+                parent_hash[curr_node.right] = curr_node
+        search_queue = [(target,0)]
+        # counter = 0
+        visited = set()
+        visited.add(target)
+        while search_queue:
+            current_node, dist = search_queue.pop(0)
+            
+            if dist == k:
+                result.append(current_node.val)
+                
+#             process the current_node
+            if current_node.left and dist<k and current_node.left not in visited:
+                search_queue.append((current_node.left,dist+1) )
+                visited.add(current_node.left)
+            if current_node.right and dist<k and current_node.right not in visited:
+                search_queue.append((current_node.right, dist+1)  )
+                visited.add(current_node.right)
+            if current_node in parent_hash and dist<k and parent_hash[current_node] not in visited:
+                search_queue.append((parent_hash[current_node], dist+1)  )
+                visited.add(parent_hash[current_node])
+            # visited.add(current_node)
+        return result
+            
+            
+            
+            
+            
+        
+    
+        

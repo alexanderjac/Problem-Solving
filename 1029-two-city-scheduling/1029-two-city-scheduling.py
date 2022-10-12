777150e0954884afd7ecd364316de3c807b4d30d
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-10-12 18:43:57 -0400

    Time: 82 ms (39.18%), Space: 13.9 MB (78.00%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        """
+        [10,20],[30,200],[400,50],[30,20]
+         10(a)    30(a)     50(a)    20(a)
+            
+          10        170     -350    -10
+        
+        [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]
+         259(a)    54(a)    667(a)    184(a)    118(a)     577(a)   
+         511        -394        -259      -45      -722      -108
+            
+        """
+        heap =[]
+        heapq.heapify(heap)
+        length=  len(costs)
+        for a,b in costs:
+            heapq.heappush(heap, (b-a) )
+        cost_final = [ a for a, _ in costs ]
+        counter =0
+        result = sum(cost_final)
+        while counter <length//2:
+            ele  = heapq.heappop(heap)
+            result +=ele
+            counter +=1
+        return result
+            
+        
+        

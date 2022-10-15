4a9bc1a0e1ca36fac2f6c93d6105b505a65912d2
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-10-15 12:08:30 -0400

    Time: 73 ms (39.41%), Space: 14.1 MB (98.13%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -19,25 +19,23 @@ class Solution:
                 queue.append(curr_node.right)
                 parent_hash[curr_node.right] = curr_node
         search_queue = [(target,0)]
-        # counter = 0
         visited = set()
         visited.add(target)
         while search_queue:
             current_node, dist = search_queue.pop(0)
-            
+            visited.add(current_node)
             if dist == k:
-                result.append(current_node.val)
-                
+                result.append(current_node.val)    
 #             process the current_node
             if current_node.left and dist<k and current_node.left not in visited:
                 search_queue.append((current_node.left,dist+1) )
-                visited.add(current_node.left)
+                # visited.add(current_node.left)
             if current_node.right and dist<k and current_node.right not in visited:
                 search_queue.append((current_node.right, dist+1)  )
-                visited.add(current_node.right)
+                # visited.add(current_node.right)
             if current_node in parent_hash and dist<k and parent_hash[current_node] not in visited:
                 search_queue.append((parent_hash[current_node], dist+1)  )
-                visited.add(parent_hash[current_node])
+                # visited.add(parent_hash[current_node])
             # visited.add(current_node)
         return result
             

48182f6ea96945fde5a9d6e0236c969ec0aaa3cb
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-10-23 10:54:14 -0400

    Time: 74 ms (32.08%), Space: 15.4 MB (6.82%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,23 +8,43 @@ class Node:
 """
 
 class Solution:
+    def __init__(self):
+        self.visited  = {}
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
-            return
-        originalNode = head
-        copyDict = {}
-        while originalNode:
-            copyNode = Node(originalNode.val)
-            copyDict[originalNode] = copyNode
-            originalNode = originalNode.next
+            return None
+        if head in self.visited:
+            return self.visited[head]
+        node = Node(head.val) 
+        self.visited[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        return node
+        
+            
+            
+    
+    
         
         
-        originalNode = head
-        while originalNode:
-            copyDict[originalNode].next = copyDict[originalNode.next] if originalNode.next else None
-            copyDict[originalNode].random = copyDict[originalNode.random] if originalNode.random else None
-            originalNode = originalNode.next
-        return copyDict[head]
+        
+        
+#         if not head:
+#             return
+#         originalNode = head
+#         copyDict = {}
+#         while originalNode:
+#             copyNode = Node(originalNode.val)
+#             copyDict[originalNode] = copyNode
+#             originalNode = originalNode.next
+        
+        
+#         originalNode = head
+#         while originalNode:
+#             copyDict[originalNode].next = copyDict[originalNode.next] if originalNode.next else None
+#             copyDict[originalNode].random = copyDict[originalNode.random] if originalNode.random else None
+#             originalNode = originalNode.next
+#         return copyDict[head]
             
             
             

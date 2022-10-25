cd5344a8693b04db00173d62bce379557d749eb9
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-10-24 21:55:50 -0400

    Time: 837 ms (92.96%), Space: 20.5 MB (39.17%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,26 +1,14 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # # sol3 with optimized heap approach
-        # heap = [(-(self.squarefunc(points[i])), i) for i in range(k)]
-        # heapq.heapify(heap)
-        # for i in range(k, len(points)):
-        #     dist = -self.squarefunc(points[i])
-        #     if dist>heap[0][0]:
-        #         heapq.heappushpop(heap, (dist,i))
-        # return [points[i] for _, i in (heap) ]
-        heap = []
+        heap = [( -(self.squarefunc(points[i])), i) for i in range(k)]
         heapq.heapify(heap)
-        for i in range(len(points)):
-            heapq.heappush(heap,(self.squarefunc(points[i]), i))
-        result = []
-        for i in range(k):
-            _, idx = heapq.heappop(heap)
-            result.append(points[idx])
-        return result
-            
-            
-            
-            
-    
+        for i in range(k, len(points)):
+            dist = -self.squarefunc(points[i])
+            if dist>heap[0][0]:
+#               got this above line?  
+                heapq.heappushpop(heap, (dist, i))
+        return [points[i] for _, i in heap]
+        
     def squarefunc(self, point):
         return point[0]**2 + point[1]**2
+        

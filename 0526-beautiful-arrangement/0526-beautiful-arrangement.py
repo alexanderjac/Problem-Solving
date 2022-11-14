599fd9043258260c8167c6e9cb40ea9d4de5ed37
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-11-13 22:25:48 -0500

    Time: 2178 ms (46.21%), Space: 14.1 MB (29.28%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,6 +1,21 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
         self.count = 0
+        self.helper(self.count,n, 1, [])
+        return self.count
+    def helper(self,count, n, idx, visited):
+        if len(visited) == n:
+            self.count+=1
+            return
+        for i in range(1, n+1):
+            if i not in visited and( i%idx ==0 or idx%i == 0):
+                visited.append(i)
+                self.helper(self.count, n,  idx+1, visited)
+                visited.pop()
+"""
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        self.count = 0
         self.backtrack(n, 1, [])
         return self.count
         
@@ -14,3 +29,6 @@ class Solution:
                 temp.append(i)
                 self.backtrack(N, idx+1, temp)
                 temp.pop()
+
+
+"""

c89f2ef2794d5bfdaa18ffd718a8a2428a740737
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-11-21 14:08:00 -0500

    Time: 210 ms (64.13%), Space: 18.1 MB (41.38%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,14 +5,12 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # print(lists)
         heap = []
         for i in range(len(lists)):
             if lists[i]:
                 heap.append((lists[i].val,i))
 
         heapq.heapify(heap)
-        # head = heapq.heappop(heap)
         head = current = ListNode()
         while heap:
             idx = heapq.heappop(heap)[1]
@@ -22,9 +20,4 @@ class Solution:
             if lists[idx]:
                 heapq.heappush(heap,(lists[idx].val, idx))
         return head.next
-        
-#         while heap:
-#             _, curr = heapq.heappop(heap)
-#             head.next = curr
-#             head = head.next
-#         return head
+ 

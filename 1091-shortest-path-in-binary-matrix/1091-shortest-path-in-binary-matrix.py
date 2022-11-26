e93b94fceeda7b8bd972859e937dbffe3218b4cd
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-11-26 18:31:57 -0500

    Time: 538 ms (99.05%), Space: 14.3 MB (69.96%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,38 +1,37 @@
 class Solution:
-#     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-#     #     sr = 0
-#     #     sc = 0
-    
-#         rowsize = len(grid)
-#         colsize = len(grid[0])
-#         tr, tc = rowsize-1, colsize-1
-#         queue = [(0, 0, 1)]
-#         visited = set()
-#         while queue:
-#             r, c, d = queue.pop()
-#             # if r == rowsize -1 and c== colsize-1:
-#             #     return d
-#             visited.add((r,c))
-#             for dr, dc in ((0,1), (1,0), (0,-1), (-1,0),(1,1), (-1,1), (1,-1),(-1,-1)):
-#                 row = r+dr
-#                 col = c+dc
-#                 if 0<=row<rowsize and 0<=col<colsize and grid[row][col] == 0 and (row,col) not in visited:
-#                     if row == rowsize-1 and col ==colsize-1 :
-#                         return d+1
-#                     queue.append((row, col,d+1))
-#         return -1
-
-    def shortestPathBinaryMatrix(self,grid):
-        n = len(grid)
-        if grid[0][0] or grid[n-1][n-1]:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:  
+        rowsize = len(grid)
+        colsize = len(grid[0])
+        if grid[0][0] or grid[rowsize-1][colsize-1]:
             return -1
-        q = [(0, 0, 1)]
-        grid[0][0] = 1
-        for i, j, d in q:
-            if i == n-1 and j == n-1: return d
-            for x, y in ((i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)):
-                if 0 <= x < n and 0 <= y < n and not grid[x][y]:
-                    grid[x][y] = 1
-                    q.append((x, y, d+1))
+        queue = [(0, 0, 1)]
+        # visited = set()
+        grid[0][0] =1
+        while queue:
+            r, c, d = queue.pop(0)
+            if r == rowsize -1 and c== colsize-1:
+                return d
+            # visited.add((r,c))
+            for dr, dc in ((0,1), (1,0), (0,-1), (-1,0),(1,1), (-1,1), (1,-1),(-1,-1)):
+                row = r+dr
+                col = c+dc
+                if 0<=row<rowsize and 0<=col<colsize and grid[row][col] == 0 :
+
+                    grid[row][col] =1
+                    queue.append((row, col,d+1))
         return -1
+
+#     def shortestPathBinaryMatrix(self,grid):
+#         n = len(grid)
+#         if grid[0][0] or grid[n-1][n-1]:
+#             return -1
+#         q = [(0, 0, 1)]
+#         grid[0][0] = 1
+#         for i, j, d in q:
+#             if i == n-1 and j == n-1: return d
+#             for x, y in ((i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)):
+#                 if 0 <= x < n and 0 <= y < n and not grid[x][y]:
+#                     grid[x][y] = 1
+#                     q.append((x, y, d+1))
+#         return -1
 

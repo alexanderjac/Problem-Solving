e1a0f9ab3ad1071a6d30bac50db75edbda13726d
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2022-12-09 19:44:37 -0500

    Time: 693 ms (88.60%), Space: 28.9 MB (95.79%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,4 +1,14 @@
 class Solution:
+    def ispalindrome(self, s):
+        left = 0
+        right = len(s)-1
+        while left <=right  :
+            if s[left]!= s[right]:
+                return False
+            left +=1
+            right -=1
+        return True
+    
     def partition(self, s: str) -> List[List[str]]:
         res = []
         def dp(s, temp, res):
@@ -6,7 +16,13 @@ class Solution:
                 res.append(temp[::])
                 return
             for i in range(1, len(s)+1):
-                if s[:i] == s[i-1::-1]:
+                # if s[:i] == s[i-1::-1]:
+                #     # print()
+                #     temp.append(s[:i])
+                #     dp(s[i:], temp, res)
+                #     temp.pop()
+                if self.ispalindrome(s[:i]):
+                    # print()
                     temp.append(s[:i])
                     dp(s[i:], temp, res)
                     temp.pop()

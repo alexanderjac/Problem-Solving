4969cc1032525252b0bc901c73dced96a6f3e0e6
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-01-10 22:18:42 -0500

    Time: 45 ms (79.02%), Space: 14.1 MB (31.03%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,4 +1,14 @@
 class Solution:
+    def flip(self,arr, k):
+        start = 0 
+        end = k -1
+        while start<=end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start +=1
+            end -=1
+        return arr
+        
+        
     def pancakeSort(self, A):
         result, n = [], len(A)
         for i in range(n,0,-1):
@@ -8,8 +18,8 @@ class Solution:
             if pl == i-1: continue
             if pl != 0:
                 result.append(pl+1)
-                A[:pl+1] = A[:pl+1][::-1]
+                A = self.flip(A, pl+1)
             result.append(i)
-            A[:i] = A[:i][::-1]
+            self.flip(A, i)
             
         return result

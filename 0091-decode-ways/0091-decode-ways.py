4b69b0a763abcfe907314898ebf80ebc63746124
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-01-29 14:00:12 -0500

    Time: 38 ms (56.80%), Space: 13.8 MB (75.24%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,11 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = {len(s) : 1}
-        for i in range(len(s)-1, -1, -1):
-            if s[i] == '0':
-                dp[i] = 0
-            else:
-                dp[i] = dp[i+1]
-            if i+1< len(s) and( s[i] == '1' or s[i] == '2' and s[i+1] in "0123456"):
-                dp[i] += dp[i+2]
-        return dp[0]
+        if s[0]=='0': # invalud str must return 0
+            return 0
+        two_p_back = 1
+        one_p_back = 1
+        for i in range(1, len(s)):
+            current = 0
+            if s[i]!='0':
+                current = one_p_back # making sure 0 case count
+            last_two_digit = int(s[i-1:i+1]) # taking current digit n prev digit
+            # 326 -> 32(invalid) | 3,26 (valid) | 3,2,6 (valid)
+            if 10<= last_two_digit <= 26 :
+                current += two_p_back # as last two digits are valid, it has to add all possibilities of last two pointers back
+            two_p_back = one_p_back # now this one pointer back will move, and becomes two
+            one_p_back = current
+        return one_p_back

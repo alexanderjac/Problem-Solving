357182be8a1f723c987d9378cb225157c6a601d4
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-03-14 13:49:25 -0400

    Time: 2479 ms (12.46%), Space: 28.9 MB (64.04%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -10,17 +10,33 @@ class Solution:
                 high = mid  
             else:
                 low = mid +1
-            
+        return low 
+    def posRight(self, arr, target, low, high):
+        while low<high:
+            mid = low + (high -low)//2
+            if arr[mid] > target:
+                high = mid  
+            else:
+                low = mid +1
 #         for i in range(low,high):
 #             if arr[i]>= target:
 #                 return i 
-        return low  
+        return low 
+
+# def bisect_right(self, a, x):
+# 		'''returns i where all a[:i] is less than or equal to x'''
+#         lo, hi = 0, len(a)
+#         while lo < hi:
+#             mid = lo + (hi - lo) // 2
+#             if a[mid] > x: hi = mid
+#             else: lo = mid + 1
+#         return lo
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
         result = 0
         for i in range(len(nums)):
             num = nums[i]
-            result += bisect_right(nums, upper-num, i+1, len(nums))  - self.posLeft(nums, lower-num, i+1, len(nums))
+            result += self.posRight(nums, upper-num, i+1, len(nums))  - self.posLeft(nums, lower-num, i+1, len(nums))
         
         return result
         nums.sort()

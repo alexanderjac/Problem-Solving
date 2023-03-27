1790e5df121dd4f97d16bf2ccc4167262e6a0ffa
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-03-27 13:23:42 -0400

    Time: 995 ms (53.73%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -7,6 +7,14 @@
 
 # select count(*) from cte where count>=3
 
-select distinct a.num as ConsecutiveNums from logs a join logs b  
-on a.id = b.id+1 and a.num = b.num 
-join logs c on a.id = c.id+2 and a.num = c.num
+with cte as (
+select id, num,lag(num,1) over(order by id) as l1, lag(num,2) over(order by id) as l2 from Logs
+    group by id,num
+)
+select distinct num as ConsecutiveNums from cte 
+where num = l1 and num = l2
+
+
+# select distinct a.num as ConsecutiveNums from logs a join logs b  
+# on a.id = b.id+1 and a.num = b.num 
+# join logs c on a.id = c.id+2 and a.num = c.num

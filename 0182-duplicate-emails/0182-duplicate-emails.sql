cbdbf322580746a4827529ee6743b05f26c6aff5
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-03-27 13:39:29 -0400

    Time: 624 ms (77.34%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,4 @@
-# Write your MySQL query statement below
-
-with cte as (
-select email, count(email) as countEmail from Person
+/* Write your T-SQL query statement below */
+select email from Person
 group by email
-)
-select email from cte where countEmail>1
+having count(Email)>1

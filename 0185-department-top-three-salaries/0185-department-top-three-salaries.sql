f841cc2ef204a885fbb9ae6b440b96ae8bacaa8f
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-03-27 16:54:53 -0400

    Time: 2957 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,33 +1,33 @@
 # Write your MySQL query statement below
 
-with result as (
-select 
-        d.name as Department , e.name as Employee, e.salary, dense_rank() over (partition by departmentID order by salary desc) as salaryRank
-From
-      Department d join Employee e
-      on d.id = e.departmentId
-)
-
-select 
-        Department, Employee, salary
-From 
-        result
-where   
-        salaryRank<=3
-
-
+# with result as (
 # select 
-#     d.name as Department, E.name as Employee, E.Salary       
-# From 
-#      Department d join Employee E
-#      on d.id = E.departmentID
-# where (departmentID, salary) in (
-
-# select 
-#      departmentId, (select Salary 
-#                     from Employee E
-#                     where E.departmentId = d.departmentId
-#                     order by Salary desc
-#                     Limit 3)
-#     from Employee d
+#         d.name as Department , e.name as Employee, e.salary, dense_rank() over (partition by departmentID order by salary desc) as salaryRank
+# From
+#       Department d join Employee e
+#       on d.id = e.departmentId
 # )
+
+# select 
+#         Department, Employee, salary
+# From 
+#         result
+# where   
+#         salaryRank<=3
+
+
+SELECT
+    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+FROM
+    Employee e1
+        JOIN
+    Department d ON e1.DepartmentId = d.Id
+WHERE
+    3 > (SELECT
+            COUNT(DISTINCT e2.Salary)
+        FROM
+            Employee e2
+        WHERE
+            e2.Salary > e1.Salary
+                AND e1.DepartmentId = e2.DepartmentId
+        )

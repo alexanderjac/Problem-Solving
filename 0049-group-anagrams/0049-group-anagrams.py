75a931537ad8742f231b9c02d5073a3138daaff8
Jackson Alexander <90269638+alexanderjac@users.noreply.github.com>
2023-12-18 19:32:16 -0500

    Time: 143 ms (6.12%), Space: 19.6 MB (70.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -25,38 +25,3 @@ class Solution:
             hash[temp].append(string)
         result = list(hash.values())
         return result
-    
-    
-class Solution:
-    def helper(self, word): 
-        if not word:
-            return ""
-        
-        max_size = 26
-        string_array = [0 for i in range(max_size)]
-        
-        for char in word:
-            string_array[ord(char) - ord('a')] += 1
-        
-        string = ""
-        for idx, value in enumerate(string_array):
-            if value != 0:
-                for i in range(value):
-                    string += chr(ord('a') + idx)
-        
-        return string
-    
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash = defaultdict(list)
-        
-        for string in strs:
-            temp = self.helper(string)
-            hash[temp].append(string)
-        
-        result = list(hash.values())
-        return result
-
-# Test case
-solution = Solution()
-result = solution.groupAnagrams([""])
-print(result)
